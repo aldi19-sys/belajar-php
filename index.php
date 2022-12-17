e96750304c64ad693f19472e5a9537c5c70c510d
aldi19-sys <aldifebriansyah1205@gmail.com>
2022-12-17 12:09:55 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo "hello world!<br>";
+
+$nama = "Aldi Febriansyah";
+
+$umur = "21";
+
+echo "Perkenalkan nama saya $nama, saya berumur $umur tahun <br>";
+
+$namaponakan = "zikri";
+
+$umurponakan = "5";
+
+echo "Nama ponakan saya $namaponakan, ponakan saya berumur $umurponakan tahun <br>";
+
+$selisihumur = $umur - $umurponakan ;
+
+echo "jadi selisih umur saya dengan ponakan saya adalah $selisihumur tahun ";
+?>
